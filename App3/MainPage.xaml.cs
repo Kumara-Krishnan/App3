0bc99134a3d164b1bb7d90b7c932c7bd048f914e
Kumara Krishnan <kumarakrishnanps@gmail.com>
2017-09-11 17:12:03 +0530

    Issue With KeepItemsInView

--- App3/MainPage.xaml.cs
+++ App3/MainPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +15,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Uwp.UI;
+using Microsoft.Toolkit.Uwp.UI.Extensions;
+using Windows.UI;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -20,11 +27,84 @@ namespace App3
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(storage, value))
+            {
+                storage = value;
+                RaisePropertyChanged(propertyName);
+            }
+        }
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null) =>
+           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion
+
+        private PersonManager DataManager;
+
+        private ObservableCollection<Person> _Persons;
+        public ObservableCollection<Person> Persons
+        {
+            get { return _Persons; }
+            set { Set(ref _Persons, value); }
+        }
+
         public MainPage()
         {
             this.InitializeComponent();
+            DataManager = new PersonManager();
+            Persons = new ObservableCollection<Person>();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SetPersonsView();
+        }
+
+        private void SetPersonsView()
+        {
+            Persons.Clear();
+            var persons = DataManager.GetPersons();
+            persons.ForEach(Persons.Add);
+            SetSelected();
+            AddNewPersons();
+
+        }
+
+        private void AddNewPersons()
+        {
+            var newPersons = DataManager.GetNewPersons();
+            for (var i = 0; i < newPersons.Count; i++)
+            {
+                Persons.Insert(i, newPersons[i]);
+            }
+        }
+
+        private void Click_Click(object sender, RoutedEventArgs e)
+        {
+            SetSelected();
+        }
+
+        private void SetSelected()
+        {
+            var random = new Random();
+            var selected = Persons.FirstOrDefault(per => per.IsSelected == true);
+            if (selected != null)
+                selected.IsSelected = false;
+            int num = random.Next(0, Persons.Count - 1);
+            Persons[num].IsSelected = true;
+            PersonView.ScrollIntoView(Persons[num], ScrollIntoViewAlignment.Leading);
+        }
+
+        private async void PersonView_Loaded(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
---
+++ App3/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace App3
+{
+    public class Person : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(storage, value))
+            {
+                storage = value;
+                RaisePropertyChanged(propertyName);
+            }
+        }
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null) =>
+           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion
+
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        private bool _IsSelected;
+        public bool IsSelected
+        {
+            get { return _IsSelected; }
+            set { Set(ref _IsSelected, value); }
+        }
+
+        public SolidColorBrush GetBackground(bool IsSelected)
+        {
+            return (IsSelected) ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Colors.Transparent);
+        }
+    }
+}
